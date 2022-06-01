027fdf50b3b3e24c56cc433f737fe29c93cb085d
LDoualito <3242@holbertonschool.com>
2022-06-01 22:19:27 +0200

    debug fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,8 @@
 # fix error 500
 exec { 'fix-php-500':
+ environment => ['DIR=/var/www/html/wp-settings.php',
+                  'OLD=phpp',
+                  'NEW=php'],
   command     => 'sudo sed -i "s/$OLD/$NEW/" $DIR',
   path        => ['/usr/bin', '/bin'],
   returns     => [0, 1]

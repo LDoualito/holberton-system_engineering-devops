acb34f39a55c2d36d9b15510f55326c93af909a8
LDoualito <3242@holbertonschool.com>
2022-06-09 11:47:19 +0200

    debugg

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,9 @@
 # increase open files limitation
 
 exec { 'fix-ngix':
+environment => ['DIR=/etc/default/nginx',
+                  'OLD=ULIMIT="-n 10"',
+                  'NEW=ULIMIT="-n 20000"'],
     command => 'sudo sed -i "s/$OLD/$NEW/" $DIR; sudo service nginx restart',
     path    => ['/usr/bin', '/bin'],
     returns => [0, 1]
